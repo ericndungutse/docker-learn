47689586975a991afeb840c7e761180ca85ad813
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-08-31 18:25:33 +0300

    setup deployment

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,10 @@ FROM node:18
 WORKDIR /usr/src/app
 
 COPY package*.json ./
+ENV NODE_ENV=production
 
 RUN npm install
+
 
 COPY . .
 
--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 import express from "express";
-import mongoose from "mongoose";
 
-import User from "./models/user.model.js";
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app  = express();
-
-mongoose.connect("mongodb+srv://eric_ndungutse:f9lvMMnMVGixPqjO@cluster0.dqpybc4.mongodb.net/docker_db?retryWrites=true&w=majority").then(()=>{
-  console.log("DB connected")
-}).catch((error)=> console.log(error.message))
+const app = express();
+// mongoose.connect("mongodb+srv://eric_ndungutse:f9lvMMnMVGixPqjO@cluster0.dqpybc4.mongodb.net/docker_db?retryWrites=true&w=majority").then(()=>{
+//   console.log("DB connected")
+// }).catch((error)=> console.log(error.message))
 
 app.get('/', function (req, res) {
   res.send('Hello World Again!')
 });
-app.get('/users', async (req,res)=>{
-  try {
-    const users = await User.find()
+console.log(process.env.NODE_ENV)
 
-    console.log(users)
+app.get('/users', async (req, res) => {
+  try {
+    console.log(process.env.SECRET_KEY)
 
     res.status(200).json({
       status: "success",
-      data: users
+      data: "Users"
     })
   } catch (error) {
     res.status(500).json({
       status: "error",
       message: "Interna server errror"
     })
-    
+
   }
 })
 
